77add862d87ebdae1afe02ac29c4232d058a88ea
Md Asjad <mdasjad895@gmail.com>
2022-12-24 17:02:24 +0530

    11

--- ML_DEPLOYMENT_MODELS/webApp.py
+++ ML_DEPLOYMENT_MODELS/webApp.py
@@ -10,20 +10,20 @@ from PIL import Image
 import urllib.request
 st.set_page_config(page_title="BANDORA LOAN APPROVAL WEBAPP", page_icon="random", layout="wide", initial_sidebar_state="expanded")
 
-def add_bg_from_local(image_file):
-    with open(image_file, "rb") as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-        st.markdown(
-            f"""
-            <style>.stApp {{background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-            background-size: cover}}
-            </style>""", unsafe_allow_html=True
-        )
-
-
-# app background
-#image=Image.open('bandora_logo.PNG')
-add_bg_from_local("bckg.jpg")
+# def add_bg_from_local(image_file):
+#     with open(image_file, "rb") as image_file:
+#         encoded_string = base64.b64encode(image_file.read())
+#         st.markdown(
+#             f"""
+#             <style>.stApp {{background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+#             background-size: cover}}
+#             </style>""", unsafe_allow_html=True
+#         )
+#
+#
+# # app background
+# #image=Image.open('bandora_logo.PNG')
+#add_bg_from_local("bckg.jpg")
 
 classifier_pipeline = pickle.load(open('classifier_gboost_pipeline.pkl', 'rb'))
 
